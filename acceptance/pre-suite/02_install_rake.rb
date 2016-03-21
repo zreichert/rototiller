5d5715740fb289c26549b7af99e159de9b04f865
Zach Reichert <zach.reichert@puppetlabs.com>
2016-03-21 11:36:08 -0700

    (QA-2229) First acceptance tests for rototiller

---
+++ acceptance/pre-suite/02_install_rake.rb
@@ -0,0 +1,4 @@
+sut = find_only_one('agent')
+on(sut, 'gem install rake')
+
+#TODO in the future use bundler to determine the version of rake
---
+++ acceptance/tests/basic_functionality/provide_default_ENV.rb
@@ -0,0 +1,47 @@
+#init
+task_name = 'Provide_a_default'
+path_to_rakefile = '/root/Rakefile'
+
+env_name = 'FOOBAR'
+env_value = 'foobaz'
+env_description = "This is a helpful description of #{env_name}"
+
+command_output = "The value of #{env_name} is #{env_value}"
+test_command = "echo '#{command_output}'"
+
+sut = find_only_one('agent')
+
+rototiller_output_regex = /WARNING: the ENV #{env_name} is not set, proceeding with default value: #{env_value}/
+
+teardown do
+
+  step 'Remove Rakefile on SUT'
+  on(sut, "rm -f #{path_to_rakefile}" )
+end
+
+rakefile = <<-EOS
+$LOAD_PATH.unshift('/root/rototiller/lib')
+require 'rototiller'
+
+Rototiller::Task::RototillerTask.define_task :#{task_name} do |t|
+  t.add_env('#{env_name}', '#{env_description}', '#{env_value}')
+  t.command = "#{test_command}"
+end
+EOS
+
+step 'Copy rake file to SUT'
+create_remote_file(sut, path_to_rakefile, rakefile)
+
+step 'Execute task defined in rake task'
+on(sut, "rake #{task_name}", :accept_all_exit_codes => true) do |result|
+
+  # exit code & no error in output
+  assert(result.exit_code == 0, 'The expected exit code 0 was not observed')
+  assert_no_match(/error/i, result.output, 'An unexpected error was observed')
+
+  # validate notification to user of ENV value
+  assert_match(rototiller_output_regex, result.stdout, 'The expected messaging was not observed')
+
+  # Use test command output to validate value of ENV used by task
+  assert_match(/#{command_output}/, result.stdout, 'The observed value of the ENV was different than expected')
+end
