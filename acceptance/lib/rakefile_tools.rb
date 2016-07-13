4942c5e8530e43d36d02a91287702a451c08edb3
Eric Thompson <eric.thompson@puppetlabs.com>
2016-07-13 11:57:43 -0700

    (QA-2478) remove step blocks in modules
    steps with blocks in modules causes beaker to not un-indent the logging
    properly, remove these.  :-(
    
    [skip ci]

--- acceptance/lib/rakefile_tools.rb
+++ acceptance/lib/rakefile_tools.rb
@@ -1,16 +1,16 @@
 module RakefileTools
+
   def create_rakefile_on(sut, rakefile_contents)
-    step 'Copy rake file to SUT' do
-      path_to_rakefile = '/root/Rakefile'
+    # using blocks for step in here causes beaker to not un-indent log
+    step 'Copy rake file to SUT'
+    path_to_rakefile = '/root/Rakefile'
 
-      teardown do
-        step 'Remove Rakefile on SUT' do
-          on(sut, "rm -f #{path_to_rakefile}" )
-        end
-      end
-
-      create_remote_file(sut, path_to_rakefile, rakefile_contents)
-      return path_to_rakefile
+    teardown do
+      step 'Remove Rakefile on SUT'
+      on(sut, "rm -f #{path_to_rakefile}" )
     end
+
+    create_remote_file(sut, path_to_rakefile, rakefile_contents)
+    return path_to_rakefile
   end
 end
