18abed49d59d80246f1f338d9039639ed40abafa
Eric Thompson <eric.thompson@puppetlabs.com>
2016-10-31 16:44:48 -0700

    (maint) fix rake install on suts
    * wow.  we need bundler
      * when back-revving from 'system' rake, we need bundler or all hell breaks loose.
    * we should probably put the bundle path in the... path so we don't have
    to use bundle exec
    * installing local gems from files is super painful.  WHY?

--- acceptance/pre-suite/01_install_rototiller.rb
+++ acceptance/pre-suite/01_install_rototiller.rb
@@ -8,5 +8,17 @@ sut = find_only_one('agent')
 `gem build rototiller.gemspec`
 scp_to(sut, gem_name, gem_name)
 
-# use force, as we may have to clobber system rake
-on(sut, "gem install --force ./#{gem_name}")
+rake_version = `rake --version`.split[2]
+# <sigh> we need to use bundler here to avoid conflicts when installing
+#   older versions of rake
+#   holy hell, this is painful
+on(sut, 'gem install --force bundler')
+# fetch rake so we can install from local cache
+on(sut, "gem fetch rake -v #{rake_version}")
+create_remote_file(sut, 'Gemfile', <<-GEMFILE)
+  source 'https://rubygems.org'
+  gem 'rake', '~> #{rake_version}'
+  gem 'rototiller'
+GEMFILE
+# config local cache to find the two gems
+on(sut, 'bundle config cache_path ./ && bundle install')
